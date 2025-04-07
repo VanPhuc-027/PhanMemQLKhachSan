48a23ceb803edcd42d4ecd42ec5244354a365d2b
ThongHoaiQuy <hoaiquy33@gmail.com>
2025-04-07 23:28:21 +0700

    Day code len

---
+++ QlKhachSan/DangNhap.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QlKhachSan
+{
+    /// <summary>
+    /// Interaction logic for DangNhap.xaml
+    /// </summary>
+    public partial class DangNhap : Window
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            // Giả lập tài khoản (có thể thay bằng kiểm tra CSDL)
+            if (username == "admin" && password == "123")
+            {
+                // Mở MainWindow
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+
+                // Đóng cửa sổ đăng nhập
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không đúng!", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void ForgotPassword_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Tính năng quên mật khẩu đang được phát triển!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ QlKhachSan/QLHangHoa.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QlKhachSan
+{
+    /// <summary>
+    /// Interaction logic for QLHangHoa.xaml
+    /// </summary>
+    public partial class QLHangHoa : Window
+    {
+        public QLHangHoa()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
